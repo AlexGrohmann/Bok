6735b26bbac35d448ad1f1a06dbe46edc56ca6bf
AlexGrohmann <alex.grohmann@googlemail.com>
2024-07-28 17:51:35 +0200

    add first component

---
+++ pages/components/Welcome.jsx
@@ -0,0 +1,12 @@
+function Welcome() {
+    return ( <div>
+        <h1>
+            Willkommen
+        </h1>
+        <button>
+            Start
+        </button>
+    </div> );
+}
+
+export default Welcome;
