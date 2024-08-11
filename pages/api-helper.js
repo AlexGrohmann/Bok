002d874714005adc5ad0f894c01b9d6862103b7a
AlexGrohmann <alex.grohmann@googlemail.com>
2024-08-11 16:40:50 +0200

    add api helper

---
+++ pages/api-helper.js
@@ -0,0 +1,37 @@
+export async function getData() {
+  const url = "/api/subscribers";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export async function addData() {
+  const url = "/api/subscribers";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ HR: "test", DE: "test" }),
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,7 @@ import Welcome from "./components/Welcome";
 import { useEffect, useState } from "react";
 import Menu from "./components/Menu";
 import ItemList from "./components/ItemList";
-
-async function getData() {
-  const url = "/api/subscribers";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-    return json;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-async function addData() {
-  const url = "/api/subscribers";
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ HR: "test", DE: "test" }),
-    });
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
+import { getData } from "./api-helper";
 
 export default function Home() {
   const [step, setStep] = useState(1);
